dfe7251035cb24b2ce63514045a8d8f8729a11af
niko <asdasd>
2021-04-03 11:39:03 -0500

    wyr app working

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+
+<h1>Would you rather...</h1>
+<!-- also would work as WouldYouRather/WouldYouRather -->
+<would-you-rather 
+v-bind:question="wyrQuestion"
+v-bind:answer1="wyrAnswer1"
+v-bind:answer2="wyrAnswer2"
+v-on:answer-changed="answerChanged"
+></would-you-rather> 
+<p> {{ userSelectionMessage }}</p>
+
+  </div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+      wyrQuestion: 'Would you rather have edible spaghetti hair that regrows every night or sweat (not sweet) maple syrup?',
+      wyrAnswer1: 'Spaghetti hair',
+      wyrAnswer2: 'Maple sweat',
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Gross! You chose ${choice}`
+    }
   }
 }
 </script>
 
 <style>
+
+body {
+  background: yellow;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +50,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: tomato;
 }
 </style>
