cc3816ee6d42bf034307eed0c5eac99654ac801d
niko <asdasd>
2021-04-07 09:54:42 -0500

    about 60%(?) working wyrq app

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,68 @@
 <div id="app">
 
 <h1>Would you rather...</h1>
-<!-- also would work as WouldYouRather/WouldYouRather -->
-<would-you-rather 
-v-bind:question="wyrQuestion"
-v-bind:answer1="wyrAnswer1"
-v-bind:answer2="wyrAnswer2"
-v-on:answer-changed="answerChanged"
-></would-you-rather> 
-<p> {{ userSelectionMessage }}</p>
+
+<would-you-rather-question
+  v-for="question in questions"
+  v-bind:key="question"
+  v-bind:question="question.wyrQuestion"
+  v-bind:answer1="question.wyrAnswer1"
+  v-bind:answer2="question.wyrAnswer2"
+  v-on:answer-changed="answerChanged"
+></would-you-rather-question> 
+<div id="user-choices">
+    <h1>You would rather...</h1>
+    <ul>
+      <li v-for="question in questions"
+      v-bind:key="question"
+      v-bind:userSelectionMessage="question.userSelectionMessage"
+      
+      >{{userSelectionMessage}}</li>
+    </ul>
+    
+</div>
 
   </div>  
 </template>
 
 <script>
-import WouldYouRather from './components/WouldYouRather.vue'
+import WouldYouRatherQuestion from './components/WouldYouRatherQuestion.vue'
 
 export default {
   name: 'App',
   components: {
-    WouldYouRather
+    WouldYouRatherQuestion
   },
   data() {
     return {
-      wyrQuestion: 'Would you rather have edible spaghetti hair that regrows every night or sweat (not sweet) maple syrup?',
-      wyrAnswer1: 'Spaghetti hair',
-      wyrAnswer2: 'Maple sweat',
-      userSelectionMessage: ''
-    }
+      questions: [
+      {
+        id: 0,
+        wyrQuestion: 'Would you rather have edible spaghetti hair that regrows every night or sweat (not sweet) maple syrup?',
+        wyrAnswer1: 'Spaghetti hair',
+        wyrAnswer2: 'Maple sweat',
+        userSelectionMessage: ''
+      },
+      {
+        id: 1,
+        wyrQuestion: 'Would you rather be a reverse centaur or a reverse mermaid/merman?',
+        wyrAnswer1: 'Reverse centaur',
+        wyrAnswer2: 'Reverse mermaid/merman',
+        userSelectionMessage: ''
+      },
+      {
+        id: 2,
+        wyrQuestion: 'Would you rather have to fart loudly every time you have a serious conversation or have to burp after every kiss?',
+        wyrAnswer1: 'Farty conversation',
+        wyrAnswer2: 'Burpy kiss',
+        userSelectionMessage: ''
+      }
+
+    ]}
   },
   methods: {
     answerChanged(choice) {
-      this.userSelectionMessage = `Gross! You chose ${choice}`
+      this.userSelectionMessage = `Gross! You would rather ${choice}`
     }
   }
 }
--- src/components/WouldYouRatherQuestion.vue
+++ src/components/WouldYouRatherQuestion.vue
@@ -1,32 +1,36 @@
 <template>
   <div class="wyr">
-    <h2>Please make your choice</h2>
-
-    <h3>{{ question }}</h3>
-
-    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade">
+    <h2>Would you rather...</h2>
+    <h3>{{question}}</h3>
+    <input type="radio" v-model="choice" 
+    v-bind:value="answer1"
+    v-on:change="choiceMade">
     <label> {{answer1}} </label>
-    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+    <input type="radio" v-model="choice" 
+    v-bind:value="answer2" 
+    v-on:change="choiceMade">
     <label> {{answer2}} </label>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'WouldYouRather',
+  name: 'WouldYouRatherQuestion',
   props: {
+    id: Number,
     question: String,
     answer1: String,
     answer2: String
   },
+  emits: ['answer-changed'], //added this line to emit the change selected to app.vue parent
   data () {
     return {
       choice: ''
     }
   },
   methods: {
-    choiceMade() {
-      this.$emit('answer-changed', this.choice)
+    choiceMade(choice) {
+      this.$emit('answer-changed', choice)
     }
   }
 }
